0e5b6327ead1a3fa27ca9e44b6f187e5cacc6d3e
CallMeTerry-ZED <callmeterryv2@gmail.com>
2026-04-23 17:42:32 -0400

    Initial project commit

---
+++ FactoryGame.Client/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FactoryGame.Core;
+
+Test test = new Test();
+
+test.Print("Hello World!");
---
+++ FactoryGame.Core/Test.cs
@@ -0,0 +1,9 @@
+﻿namespace FactoryGame.Core;
+
+public class Test
+{
+    public void Print(string s)
+    {
+        Console.WriteLine(s);
+    }
+}
