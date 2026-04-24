4e1fd2ad4003ddfc98a95b7276ff7e66f92c0c0e
CallMeTerry-ZED <callmeterryv2@gmail.com>
2026-04-24 13:53:28 -0400

    Init openGL and clear color

--- FactoryGame.Client/Window.cs
+++ FactoryGame.Client/Window.cs
@@ -1,5 +1,6 @@
 using Silk.NET.Maths;
 using Silk.NET.Windowing;
+using Silk.NET.OpenGL;
 using FactoryGame.Core.Log;
 
 namespace FactoryGame.Client;
@@ -7,6 +8,7 @@ namespace FactoryGame.Client;
 public class Window
 {
     private readonly IWindow _window;
+    private GL? _gl;
 
     public Window(string title = "FactoryGame", int width = 800, int height = 600, bool vsync = false)
     {
@@ -15,12 +17,14 @@ public class Window
             Title = title,
             Size = new Vector2D<int>(width, height),
             VSync = vsync,
+            API = GraphicsAPI.Default // Default is OpenGL 3.3 core will want to look into changing this soon
         };
         
         _window = Silk.NET.Windowing.Window.Create(options);
 
         _window.Load += OnLoad;
         _window.Update += OnUpdate;
+        _window.Render += OnRender;
         _window.Closing += OnClose;
     }
     
@@ -28,12 +32,22 @@ public class Window
 
     private void OnLoad()
     {
+        _gl = GL.GetApi(_window);
+        Logger.Info($"OpenGL version: {_gl.GetStringS(StringName.Version)}");
+        Logger.Info($"GPU: {_gl.GetStringS(StringName.Renderer)}");
+        
         Logger.Info("Window loaded.");
     }
 
     private void OnUpdate(double delta)
     {
-        
+        // game loop
+    }
+    
+    private void OnRender(double delta)
+    {
+        _gl!.ClearColor(0.1f, 0.1f, 0.1f, 1.0f); // dark gray
+        _gl!.Clear(ClearBufferMask.ColorBufferBit);
     }
 
     private void OnClose()
